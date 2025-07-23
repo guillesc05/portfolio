16bb8764bc77b2469cfea9392bc0f4cd6011519e
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-07-23 14:57:43 +0200

    games

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ function openTab(evt, tabName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName).style.display = "inline";
   evt.currentTarget.className += " active";
 }
